e7de6138612d884cf9023e9e45fc446212772314
Edwin_K <edumwas911@gmail.com>
2025-01-21 14:44:54 +0300

    Design onboarding screen

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl  my-10 font-rubik">Welcome to Dreamlots</Text>
+      <Link href='/sign-in'>SignIn</Link>
+      <Link href='/profile'>Profile</Link>
+      <Link href='/explore'>Explore</Link>
+      <Link href='/properties/1'>Property</Link>
+
+    </View>
+  );
+}
